44dd30124b11cfc8ade9220ec111c30ac32d4a0e
Najib Mahfuj <22-48248-2@student.aiub.edu>
2025-07-23 02:37:19 +0600

    REST API

--- src/User/user.Controller.ts
+++ src/User/user.Controller.ts
@@ -1,6 +1,37 @@
-import { Controller } from "@nestjs/common";
+import { Body, Controller, Delete, Get, Param, Post, Put, Query } from '@nestjs/common';
+import { UserService } from './user.service';
 
 @Controller()
 export class UserController {
-  
+  constructor(private readonly userService: UserService) {}
+
+  @Get('user/getAll')
+  getAllUsers(): string {
+    return this.userService.getAllUsers();
+  }
+
+  @Get('user/get/:id')
+  getUser(@Param('id') id: string): string {
+    return this.userService.getUser(id);
+  }
+
+  @Get('user/getByName')
+  getUsers(@Query('name') name: string): string {
+    return this.userService.getUsersByName(name);
+  }
+
+  @Post('user/create')
+  createUser(@Body() data: any): string {
+    return this.userService.createUser(data);
+  }
+
+  @Put('user/:id')
+  updateUser(@Param('id') id: string, @Body() data: any): string {
+    return this.userService.updateUser(id, data);
+  }
+
+  @Delete('user/:id')
+  deleteUser(@Param('id') id: string): string {
+    return this.userService.deleteUser(id);
+  }
 }
--- src/User/user.service.ts
+++ src/User/user.service.ts
@@ -1,7 +1,28 @@
-
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class UserService {
-	
+  getAllUsers(): string {
+    return 'All users returned from service';
+  }
+
+  getUser(id: string): string {
+    return `User with ID ${id} returned from service`;
+  }
+
+  getUsersByName(name: string): string {
+    return `Name: ${name} returned from service`;
+  }
+
+  createUser(data: any): string {
+    return `User created with name: ${data.name}, username: ${data.username}`;
+  }
+
+  updateUser(id: string, data: any): string {
+    return `User with ID ${id} updated with data: ${JSON.stringify(data)}`;
+  }
+
+  deleteUser(id: string): string {
+    return `User with ID ${id} deleted`;
+  }
 }
