f937d546716c4e2bf46fb6b774961c3a14c54c7f
jianbot007 <90086537+jianbot007@users.noreply.github.com>
2025-07-23 00:03:57 +0600

    Admin Rest api

--- src/Admin/admin.service.ts
+++ src/Admin/admin.service.ts
@@ -1,14 +1,27 @@
 import { Injectable } from "@nestjs/common";
 
 @Injectable()
-export class AdminService{
-    getAdmin():string {
+export class AdminService {
+    
+    getAdmin(): string {
         return "All Admin";
     }
-      getAdminByNameandID(name:string,id:number): object{
-        return {name:name,id:id}
+
+    getAdminByNameandID(name: string, id: number): object {
+        return { name, id };
     }
-    addAdmin(admin:object):object{
+
+    addAdmin(admin: object): object {
         return admin;
+    }
+
+    deleteAdmin(id: number): string {
+        
+        return `Admin with ID ${id} has been deleted`;
+    }
+
+    editAdmin(id: number, updatedData: object): string {
+       
+        return `Admin with ID ${id} has been edited`;
     }
 }
